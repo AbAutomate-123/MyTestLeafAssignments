5d077e91121c48c92e05379a386e46ea4062ea8d
AbAutomate-123 <149084472+AbAutomate-123@users.noreply.github.com>
2023-10-27 12:25:54 +0530

    Add files via upload
    With Report added

---
+++ day1/assignment/ReportAssignement5.java
@@ -0,0 +1,16 @@
+package week1.day1.assignment;
+
+public class ReportAssignement5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentAssignment5v1 Stu= new StudentAssignment5v1();
+		
+		System.out.println("Name of the student is : " + Stu.studentName);
+		System.out.println("Roll Number is : " + Stu.rollNo);
+		System.out.println("Name of the college : " + Stu.collegeName);
+		System.out.println("Marks scored by the student is : " + Stu.marksScored);
+		System.out.println("CGPA is : " + Stu.cgpa);
+	}
+
+}
---
+++ day1/assignment/StudentAssignment5v1.java
@@ -0,0 +1,18 @@
+package week1.day1.assignment;
+
+public class StudentAssignment5v1 {
+	
+	String studentName = "Arvind";
+	int rollNo = 11;
+	String collegeName = "Best";
+	float marksScored = 98.50f;
+	float cgpa = 4.00f; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+	}
+
+}
